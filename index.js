b2d07e5de746763115c86c781d607fa034808dd9
Vinamrata Rani <vinamrata1086@gmail.com>
2021-04-01 21:23:05 +0530

    pwa

---
+++ index.js
@@ -0,0 +1,12 @@
+if('serviceWorker' in navigator){
+   navigator.serviceWorker.register('sw.js').then(registration =>{
+     console.log("SW Registered");
+     console.log(registeration);
+   }).catch(error =>{
+     console.log("SW Registration failed");
+     console.log(error);
+   });
+}
+
+
+
