5c48045bfa0484e4010d0d99fa8952615d4b98d0
Vinamrata Rani <vinamrata1086@gmail.com>
2021-04-03 11:28:37 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-if('serviceWorker' in navigator){
-   navigator.serviceWorker.register('sw.js').then(registration =>{
+if("serviceWorker" in navigator){
+   navigator.serviceWorker.register("sw.js").then(registration =>{
      console.log("SW Registered");
-     console.log(registeration);
+     console.log(registration);
    }).catch(error =>{
      console.log("SW Registration failed");
      console.log(error);
